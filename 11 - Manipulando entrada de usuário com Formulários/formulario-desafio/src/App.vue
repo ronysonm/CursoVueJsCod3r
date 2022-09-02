35fcafebf057e416a993edf289a7809da2c85803
Ronyson Martins <ronysonm@gmail.com>
2022-09-01 21:32:50 -0300

    Finalizando o cap 11

---
+++ 11 - Manipulando entrada de usuário com Formulários/formulario-desafio/src/App.vue
@@ -0,0 +1,137 @@
+<template>
+	<div id="app">
+		<h1>Formulário Desafio</h1>
+		<div class="conteudo">
+			<form class="painel" v-if="!enviado">
+				<div class="cabecalho">Formulário</div>
+				<Rotulo nome="Nome">
+					<input type="text" v-model="usuario.nome">
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<input type="text" v-model="usuario.sobrenome">
+				</Rotulo>
+				<Rotulo nome="Email">
+					<input type="email" v-model="usuario.email">
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="usuario.senha">
+				</Rotulo>
+				<Rotulo nome="Armazena Dados?">
+					<input type="checkbox" v-model="usuario.armazenaDados">
+				</Rotulo>
+
+				<!-- Exercicio 01 -->
+				<!-- Criar uma formulário de registro -->
+				<!-- Nome completo (Nome e Sobrenome) -->
+				<!-- Email -->
+				<!-- Senha -->
+				<!-- Armazenar Dados? (Sim/Não) -->
+
+				<!-- Exercicio 02 -->
+				<!-- Só mostrar o fomulário de não tiver sido submetido -->
+				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+				
+				<!-- Exercicio 03 -->
+				<!-- Crie um componente personalizado NomeCompleto -->
+				<!-- Esse componente deve receber Nome e Sobrenome -->
+				<button @click.prevent="enviar">Enviar</button>
+			</form>
+			<div class="painel" v-else>
+				<div class="cabecalho">Resultado</div>
+				<p>Nome: {{ usuario.nome }}</p>
+				<p>Sobrenome: {{ usuario.sobrenome }}</p>
+				<p>Email: {{ usuario.email }}</p>
+				<p>Senha: {{ usuario.senha }}</p>
+				<p>Armazena Dados: {{ usuario.armazenaDados }}</p>
+
+				<NomeCompleto :nome="usuario.nome" :sobrenome="usuario.sobrenome" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Rotulo from './components/Rotulo.vue'
+import NomeCompleto from '@/components/NomeCompleto.vue'
+
+export default {
+	name: 'app',
+	components: { Rotulo, NomeCompleto },
+	methods: {
+		enviar() {
+			this.enviado = true
+		}
+	},
+	data() {
+		return {
+			enviado: false,
+			usuario: {
+				nome : '',
+				sobrenome: '',
+				email: '',
+				senha: '',
+				armazenaDados: false
+			}
+		}
+	}
+}
+</script>
+
+<style>
+
+body {
+	background-color: #ECECEC;
+}
+
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+	display: flex;
+	flex-direction: column;
+}
+
+.conteudo {
+	display: flex;
+}
+
+.painel {
+	flex: 1;
+	background: #FFF;
+	margin: 0px 10px;
+	padding: 20px;
+	border: 1px solid #AAA;
+	border-radius: 5px;
+}
+
+.painel .cabecalho {
+	width: 100%;
+	background-color: #DDD;
+	padding: 10px 0px;
+	border-radius: 5px;
+	font-size: 1.4rem;
+}
+
+#app form button {
+	float: right;
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+
+#app h1 {
+	font-weight: 200;
+	margin: 20px;
+	padding: 0;
+}
+
+.mr-4 {
+	margin-right: 40px;
+}
+</style>
---
+++ 11 - Manipulando entrada de usuário com Formulários/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nome-completo">
+    <h3> Nome Completo </h3>
+    <span><strong>{{ nome }} {{ sobrenome }}</strong></span>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: { 
+            type: String,
+            required: true
+        },
+        sobrenome: {
+            type: String,
+            required: true
+        }
+    }
+
+}
+</script>
